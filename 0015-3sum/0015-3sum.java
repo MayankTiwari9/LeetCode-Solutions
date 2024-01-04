70e1d02d5f8a37625d1864e477564645d7485c27
Mayank Tiwari <103255091+MayankTiwari9@users.noreply.github.com>
2024-01-04 16:01:55 +0530

    Time: 244 ms (30.35%), Space: 51.6 MB (29.76%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -9,34 +9,29 @@ class Solution {
             return ans;
         }
         
+        if(nums[0] > 0){
+            return ans;
+        }
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
         for(int i = 0; i < n; ++i){
-            if(nums[i] > 0)
+            map.put(nums[i], i);
+        }
+        
+        for(int i = 0; i < n-2 ; ++i){
+            if(nums[i] > 0){
                 break;
-            
-            if(i > 0 && nums[i] == nums[i - 1])
-                continue;
-            
-            int low = i + 1, high = n - 1;
-            
-            while(low < high){
-                int sum = nums[i] + nums[low] +  nums[high];
-                
-                if(sum > 0)
-                    high--;
-                else if(sum < 0)
-                    low++;
-                else{
-                    ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                    int lowLastOcc = nums[low];
-                    int highLastOcc = nums[high];
-                    
-                    while(low < high && nums[low] == lowLastOcc)
-                        low++;
-                    
-                    while(low < high && nums[high] == highLastOcc)
-                        high--;
-                }
             }
+            
+            for(int j = i + 1; j < n - 1; ++j){
+                int req = -1 * (nums[i] + nums[j]);
+                if(map.containsKey(req) && map.get(req) > j){
+                    ans.add(Arrays.asList(nums[i], nums[j], req));
+                }
+                j = map.get(nums[j]);
+            }
+            i = map.get(nums[i]);
         }
         return ans;
     }
