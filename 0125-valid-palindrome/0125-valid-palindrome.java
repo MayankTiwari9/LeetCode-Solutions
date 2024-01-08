3a11d0227bb6c47f5d706587069b3e11e7e5ce3e
Mayank Tiwari <103255091+MayankTiwari9@users.noreply.github.com>
2024-01-08 12:32:12 +0530

    Time: 14 ms (41.34%), Space: 45.1 MB (12.39%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        
+        int start = 0, end = str.length()-1;
+        
+        while(start < end){
+            if(str.charAt(start) != str.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+}
