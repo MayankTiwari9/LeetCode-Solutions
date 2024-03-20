e9ef81a116f708f3a568ca653d7519d7102f7358
Mayank Tiwari <103255091+MayankTiwari9@users.noreply.github.com>
2024-03-20 16:54:25 +0530

    Time: 1 ms (74.79%), Space: 44.5 MB (85.83%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-         int j = 0;
-
-        for (int i = 0; i < nums.length-1; i++) {
-            if(nums[i] != nums[i+1]){
-                nums[j++] = nums[i];
+        // numbe ro funique elements
+        
+        int n = nums.length;
+        
+        int index = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i - 1]){
+                 nums[index] = nums[i];
+                index++;
             }
+               
         }
-
-        nums[j++] = nums[nums.length-1];
-
-        return j;
+        
+        return index;
     }
 }
